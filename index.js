620a46f8d7398eb665f62315783e00580d25af16
Jens Tinfors <jens.tinfors@gmail.com>
2020-09-28 08:46:46 +0200

    Working on WIP

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 Cypress.Commands.add('lighthouse', (url) => {
-  cy.exec(`lighthouse ${url} --output json --chrome-flags="--headless"`).then(({ stdout }) => {
+  cy.exec(`$(npm bin)/lighthouse ${url} --output json --chrome-flags="--headless"`).then(({ stdout }) => {
     const { categories } = JSON.parse(stdout);
     return Object.keys(categories).reduce((curr, key) => {
       return {
