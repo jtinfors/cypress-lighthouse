75bc3f9f39f8c8b5766f1c4d6788b7abf5d27f41
Jens Tinfors <jens.tinfors@gmail.com>
2020-09-28 09:56:39 +0200

    bla

--- index.js
+++ index.js
@@ -3,11 +3,14 @@
 Cypress.Commands.add('lighthouse', (url) => {
   cy.exec(`$(npm bin)/lighthouse ${url} --output json --chrome-flags="--headless"`).then(({ stdout }) => {
     const { categories } = JSON.parse(stdout);
-    return Object.keys(categories).reduce((curr, key) => {
+    console.log('categories => ', categories)
+    const data = Object.keys(categories).reduce((curr, key) => {
       return {
         ...curr,
         [key]: categories[key].score
       }
     }, {});
+    console.log('data => ', data)
+    return data;
   });
 });
