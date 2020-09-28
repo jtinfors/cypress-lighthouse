22c7312522d56f951ff8f3753bd2695017e17935
Jens Tinfors <jens.tinfors@gmail.com>
2020-09-28 10:10:04 +0200

    Derp search/replace fuckup

--- index.js
+++ index.js
@@ -3,14 +3,11 @@
 Cypress.Commands.add('lighthouse', (url) => {
   cy.exec(`$(npm bin)/lighthouse ${url} --output json`).then(({ stdout }) => {
     const { categories } = JSON.parse(stdout);
-    console.log('categories => ', categories)
-    const data = Object.keys(categories).reduce((curr, key) => {
+    return Object.keys(categories).reduce((curr, key) => {
       return {
         ...curr,
         [key]: categories[key].score
       }
     }, {});
-    console.log('data => ', data)
-    return data;
   });
 });
